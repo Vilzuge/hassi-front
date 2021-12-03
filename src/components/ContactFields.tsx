1014d6bbdaa73c47d6e1b049731d4f149eddf555
hugohutri <55588133+hugohutri@users.noreply.github.com>
2021-12-03 23:35:53 +0200

    remove images and improve cover

--- src/components/ContactFields.tsx
+++ src/components/ContactFields.tsx
@@ -1,28 +1,49 @@
 import { FunctionComponent } from 'react';
 
 const ContactFields: FunctionComponent = () => {
-
-
   return (
     <>
-      <div>
-        Toimitus
-      </div>
-      <div className="columns">
+      <div>Toimitus</div>
+      <div className="">
         <label htmlFor="exampleName">Nimi</label>
-        <input className="u-full-width" type="email" placeholder="" id="nameInput"/>
+        <input
+          className="u-full-width"
+          type="email"
+          placeholder=""
+          id="nameInput"
+        />
 
         <label htmlFor="exampleEmailInput">Sähköposti</label>
-        <input className="u-full-width" type="email" placeholder="" id="emailInput"/>
+        <input
+          className="u-full-width"
+          type="email"
+          placeholder=""
+          id="emailInput"
+        />
 
         <label htmlFor="exampleEmailInput">Osoite</label>
-        <input className="u-full-width" type="email" placeholder="" id="addressInput"/>
+        <input
+          className="u-full-width"
+          type="email"
+          placeholder=""
+          id="addressInput"
+        />
 
         <label htmlFor="exampleEmailInput">Postinumero</label>
-        <input className="u-full-width" type="email" placeholder="" id="exampleEmailInput"/>
+        <input
+          className="u-full-width"
+          type="email"
+          placeholder=""
+          id="exampleEmailInput"
+        />
 
         <label htmlFor="exampleEmailInput">Postitoimipaikka</label>
-        <input className="u-full-width" type="email" placeholder="" id="exampleEmailInput"/>
+        <input
+          className="u-full-width"
+          type="email"
+          placeholder=""
+          id="exampleEmailInput"
+        />
       </div>
     </>
   );
