993af16cdce2cdce9d8128b5dcccf5445cc1cb66
hugohutri <55588133+hugohutri@users.noreply.github.com>
2021-12-03 21:19:55 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Demo</header>
+    </div>
+  );
+};
+
+export default App;
